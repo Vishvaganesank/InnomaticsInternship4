f83f253ea87917633ee7470f1b22284fcd847521
VISHVA G <118836269+Vishvaganesank@users.noreply.github.com>
2025-02-16 21:26:42 +0530

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Casual Shirt",
+    price: 100,
+    desc: "Men regular fit self design cut away collar casual shirt",
+    img: "images/casual.webp",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Formal Shirt",
+    price: 125,
+    desc: "Men Regukar Fit Solid spread Collar Formal Shirt ",
+    img: "images/formal.jpeg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "T Shirt",
+    price: 45,
+    desc: "Men Printed Roundneck Polyster halfsleeve T-Shirt",
+    img: "images/Tshirt.jpeg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Mens Suit",
+    price: 300,
+    desc: "Men Wedding suite for grooms - Brown suite ",
+    img: "images/suite.webp",
+  },
+];
